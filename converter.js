71e6cbb8e065bc07a1c532c95d2b3b41c8a1416d
Taylor Shuff <rhd5538@trh.com>
2016-08-01 15:10:03 -0500

    added angular components, need to test on http-server

---
+++ converter.js
@@ -0,0 +1,19 @@
+function() {
+  
+  var app = angular.module('nameApp', []);
+}();
+
+
+var hold = document.getELementById('holder');
+var degrees = document.getELementById('deg').value;
+
+
+
+
+function toCelsius () {
+
+}
+
+function toFahrenheit () {
+
+}
