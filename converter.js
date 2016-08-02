efbabcb7c8616e7b004ad0c2512cde0b93defed2
Taylor Shuff <tkylesh@gmail.com>
2016-08-01 20:34:46 -0500

    got the degree text to display finally

--- converter.js
+++ converter.js
@@ -3,14 +3,18 @@
 
 
 
-	var hold = document.getElementById("holder").innerHtml;
-	var degrees = document.getElementById("deg").value;
+	 
 	
+	
+function displayTemp(){
+	//console.log("1234...");
+	//document.getElementById("holder").innerHTML = "test";
+	//console.log(document.getElementById("deg").value);
+	document.getElementById("holder").innerHTML =document.getElementById("deg").value;
+}
 
 function toCelsius () {
 	//(F - 32)/1.8
-
-
 
 }
 
